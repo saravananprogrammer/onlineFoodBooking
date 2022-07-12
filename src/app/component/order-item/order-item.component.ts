1112f3b0ca1f17a9bd63eb884ba7cb11fb495615
saravananprogrammer <saravanan.programnanalyst@gmail.com>
2022-07-12 14:05:21 +0530

    filter work

--- src/app/component/order-item/order-item.component.ts
+++ src/app/component/order-item/order-item.component.ts
@@ -74,10 +74,7 @@ export class OrderItemComponent implements OnInit {
 
    /** Announce the change in sort state for assistive technology. */
    announceSortChange(sortState: Sort) {
-    // This example uses English messages. If your application supports
-    // multiple language, you would internationalize these strings.
-    // Furthermore, you can customize the message to add additional
-    // details about the values being sorted.
+    
     if (sortState.direction) {
       this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
     } else {
