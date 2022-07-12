07c7cef94e67712673604dc655f16be14680f6ad
saravananprogrammer <saravanan.programnanalyst@gmail.com>
2022-07-12 13:06:11 +0530

    sort work

--- src/app/component/order-item/order-item.component.ts
+++ src/app/component/order-item/order-item.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit,ViewChild,AfterViewInit } from '@angular/core';
 import {MatTableDataSource } from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
-import {Sort} from '@angular/material/sort';
+import {MatSort, Sort} from '@angular/material/sort';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
 
 
 @Component({
@@ -16,8 +17,9 @@ export class OrderItemComponent implements OnInit {
   dataSource = new MatTableDataSource<FoodItems>(last30DaysList);
  
   //@ViewChild(MatPaginator) paginator: MatPaginator;
+  //@ViewChild(MatSort) sort: MatSort;
  
-  constructor() { }
+  constructor(private _liveAnnouncer: LiveAnnouncer) { }
 
 
   ngOnInit(): void {
@@ -25,6 +27,20 @@ export class OrderItemComponent implements OnInit {
   }
   ngAfterViewInit() {
     //this.dataSource.paginator = this.paginator;
+    //this.dataSource.sort = this.sort;
+  }
+
+   /** Announce the change in sort state for assistive technology. */
+   announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 
 }
